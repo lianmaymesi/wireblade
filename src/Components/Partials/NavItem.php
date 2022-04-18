155dad6355eea7698cd169fa55450bc504b97658
LiAn DhAs <lianmay.mesi@gmail.com>
2022-04-18 08:04:36 +0530

    Fixed minor issues

--- src/Components/Partials/NavItem.php
+++ src/Components/Partials/NavItem.php
@@ -6,15 +6,13 @@ use Illuminate\View\Component;
 
 class NavItem extends Component
 {
-    public $submenu;
-
     public $dropdown;
 
     public $title;
 
     public $route;
 
-    public function __construct($submenu = '', $dropdown = '', $title, $route = '')
+    public function __construct($dropdown = '', $title, $route = '')
     {
         $this->submenu = $submenu;
 
